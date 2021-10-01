6d3fc67bd68fe69ae810edce0e391f0f2bfad590
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2021-10-01 11:58:51 -0300

    add teacher class

---
+++ Rainbow Schools/Rainbow Schools/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow_Schools
+{
+    class Subject
+    {
+        private string _name;
+        private string _cod;
+        private string _teacher;
+
+        public Subject(string n, string c, string t)
+        {
+            this._name = n;
+            this._cod = c;
+            this._teacher = t;
+        }
+
+        public string Teacher => _teacher;
+
+        override
+        public string ToString()
+        {
+            return _cod + "\t|\t" + _name + "\t|\t" + _teacher;
+        }
+    }
+}
+}
